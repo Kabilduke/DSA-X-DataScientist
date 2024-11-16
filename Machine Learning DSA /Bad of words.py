7691efaf8db801b93a30b4dc039d4f615a6e5f3b
K A B I L <100506503+Kabilduke@users.noreply.github.com>
2024-11-16 14:56:31 +0530

    Create Bad of words.py

---
+++ Machine Learning DSA /Bad of words.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+
+def bag_of_words(s):
+  text = s.lower()
+  result = []
+  
+  for char in text:
+    if char.isalpha() or char.isspace():
+      result.append(char)
+      
+  words = ''.join(result).split()
+  Bag = Counter(words)
+  
+  return Bag
+
+
+s = 'Natural Language Processing is easy and fun to learn. Natural Language Processing help to understand the meaning of text"
+Bag_words = bag_of_words(s)
+print("Bag of Words:", Bag_words)
