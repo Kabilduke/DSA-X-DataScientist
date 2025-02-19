8690eb2662f80ce92b59d638b6c56b3665739837
K A B I L <100506503+Kabilduke@users.noreply.github.com>
2025-02-19 10:44:57 +0530

    Update and rename 311. Sparse Matrix Multiplication to 311. Sparse Matrix Multiplication.py

--- 311. Sparse Matrix Multiplication.py
+++ 311. Sparse Matrix Multiplication.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-Given two sparse matrices A and B, return the result of A * B. You can assume that A's number of columns is equal to B's number of rows.
-Given
+Given two sparse matrices A and B, return the result of A * B. You can assume that A(s) number of columns is equal to B(s) number of rows.
 A = [
   [ 1, 0, 0],
   [-1, 0, 3]
@@ -14,7 +13,11 @@ B = [
 ]
 
 Solution:
+
 a = np.array(A)
 b = np.array(b)
 
 result = np.dot(a, b)
+
+==> [[ 7  0  0]
+     [-7  0  3]]
