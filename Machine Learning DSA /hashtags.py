f29ce4560cccf60bf5c8cafa63fa5499b8c32b37
K A B I L <100506503+Kabilduke@users.noreply.github.com>
2025-07-23 08:39:29 +0530

    Create hashtags.py

---
+++ Machine Learning DSA /hashtags.py
@@ -0,0 +1,21 @@
+posts = [
+    "Loving the new features! #updates #features",
+    "Check out our latest blog post! #Blog #update",
+    "#Throwback to our first launch day! #throwback #launch",
+    "Working on bug fixes. #update #BugFix", 
+    "Another day, another deployment. #update #DevOps"
+]
+
+
+import re
+from collections import Counter
+
+tags = []
+
+for post in posts:
+    hastags = re.findall(r'#(\w+)',post)
+    tags.append(hastags[0].lower())
+
+hastag_counts = Counter(tags)
+most_common = hastag_counts.most_common()
+print("Most commonly used hastags:",most_common[0][0])
